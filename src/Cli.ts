df118b9ca674cf20bde9cdd1c02deb6ef4cc5741
Karmbir Chima <karmbirchima@gmail.com>
2024-08-26 05:00:57 -0700

    set up create method

---
+++ src/Cli.ts
@@ -0,0 +1,55 @@
+import { Forest } from "./Forest";
+import { Tree } from "./Tree";
+import { Node } from "./Node";
+import { isPathValid } from "./utils";
+
+export class Cli {
+    protected forest: Forest;
+    public multilineMode: boolean = false;
+    public logQueue: string[] = [];
+
+    constructor() {
+        this.forest = new Forest();
+    }
+
+    public getForest(): Forest {
+        return this.forest;
+    }
+
+    public setMultiLineMode(multilineMode: boolean) {
+        this.multilineMode = multilineMode;
+        if (!multilineMode) this.flushLogQueue();
+    }
+
+    create(directoryPath: string, { fromCli = false } = {}) {
+        const { treeName, splitPath, error } = this.parsePath(directoryPath);
+        if (error) return error;
+
+        let tree = this.forest.trees[treeName];
+        if (!tree) {
+            const root = new Node(treeName);
+            tree = new Tree(root);
+            this.forest.addTree(tree);
+        }
+
+        let currentNode = tree.root;
+
+        const pathIsValid = isPathValid(splitPath, currentNode);
+        if (!pathIsValid.isValid) {
+            return this.logAndReturnError(
+                `Invalid path: ${pathIsValid.missingNode} does not exist`,
+                { fromCli }
+            );
+        }
+
+        for (let i = 1; i < splitPath.length; i++) {
+            const directoryName = splitPath[i];
+            if (!currentNode.children[directoryName]) {
+                const newNode = new Node(directoryName);
+                currentNode.addChild(newNode);
+            }
+            currentNode = currentNode.children[directoryName];
+        }
+        this.checkForMultilineMode(`CREATE ${directoryPath}`, { fromCli });
+    }
+}
