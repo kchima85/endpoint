8f6fc9cfb1012331ca63ef1506ab6fc56123ae18
Karmbir Chima <karmbirchima@gmail.com>
2024-08-26 04:59:34 -0700

    set up move tests

--- __tests__/Cli.test.ts
+++ __tests__/Cli.test.ts
@@ -46,4 +46,87 @@ describe("Cli", () => {
             expect(output).toBe(structure);
         });
     });
+
+    describe("move", () => {
+        it("should move a leaf directory within the same tree", () => {
+            const cli = new Cli();
+            cli.create("foo");
+            cli.create("foo/bar");
+            cli.create("foo/bar/baz");
+            cli.create("foo/baz");
+            cli.move("foo/bar/baz", "foo/baz");
+
+            const forest = cli.getForest();
+            expect(
+                forest.trees["foo"].root.children["baz"].children["baz"]
+            ).toBeDefined();
+        });
+
+        it("should move a directory with children within the same tree", () => {
+            const cli = new Cli();
+            cli.create("foo");
+            cli.create("foo/bar");
+            cli.create("foo/bar/baz");
+            cli.create("foo/baz");
+            cli.move("foo/bar", "foo/baz"); // bar/baz should be moved to foo/baz/bar/baz
+
+            const forest = cli.getForest();
+            expect(
+                forest.trees["foo"].root.children["baz"].children["bar"]
+                    .children["baz"]
+            ).toBeDefined();
+        });
+
+        it("should move a leaf directory to a different tree", () => {
+            const cli = new Cli();
+            cli.create("foo");
+            cli.create("foo/bar");
+            cli.create("foo/bar/baz");
+            cli.create("baz");
+            cli.move("foo/bar/baz", "baz");
+
+            const forest = cli.getForest();
+            expect(forest.trees["baz"].root.children["baz"]).toBeDefined();
+        });
+
+        it("should move a directory with children to a different tree", () => {
+            const cli = new Cli();
+            cli.create("foo");
+            cli.create("foo/bar");
+            cli.create("foo/bar/baz");
+            cli.create("baz");
+            cli.move("foo/bar", "baz");
+
+            const forest = cli.getForest();
+            expect(
+                forest.trees["baz"].root.children["bar"].children["baz"]
+            ).toBeDefined();
+        });
+
+        it("should not move a directory if the directory already exists", () => {
+            const cli = new Cli();
+            cli.create("foo");
+            cli.create("foo/bar");
+            cli.create("foo/bar/baz");
+            cli.create("foo/baz");
+
+            const expectedError = cli.move("foo/bar/baz", "foo");
+            expect(expectedError).toBe(
+                "Invalid destination path: baz already exists in foo"
+            );
+        });
+
+        it("should not move a directory if the destination path is invalid", () => {
+            const cli = new Cli();
+            cli.create("foo");
+            cli.create("foo/bar");
+            cli.create("foo/bar/baz");
+            cli.create("foo/baz");
+
+            const expectedError = cli.move("foo/bar/baz", "invalid/path");
+            expect(expectedError).toBe(
+                "Invalid path: foo/bar/baz or invalid/path does not exist"
+            );
+        });
+    });
 });
